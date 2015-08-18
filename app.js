c789768eaddd455ef4f3776413754ffa0c941018
bennetto <bbonnetto@gmail.com>
2015-08-18 08:31:26 +0200

    add multiple bulle

--- app.js
+++ app.js
@@ -11,39 +11,51 @@ function getValueInCss(style){
 }
 
 
-function bulle(){
+function bulle(_positionX,_positionY,_maxMove,temps){
 
     var self = this;
     var div = document.createElement("div");
+    
+    var position = {};
+    position.x= _positionX;
+    position.y= _positionY;
+    
+    var move = {};
+    move.x = 0;
+    move.y=0;
+    
+    var maxMove = _maxMove; 
 
-    var x =10;
-    var y = 10;
-    var tempsTransition = 2; // en second
+    var tempsTransition = temps; // en second
 
+    div.style.position = "absolute";
     div.style.backgroundColor = "turquoise";
     div.style.height = "50px";
     div.style.width = "50px";
     div.style.borderRadius = "50px";
-    div.style.WebkitTransition = '-webkit-transform '+tempsTransition+'s';
-
+    div.style.WebkitTransition = '-webkit-transform '+tempsTransition+'s linear';
+    div.style.marginLeft = position.x+'px';
+    div.style.marginTop = position.y+'px';
 
     var refresh = function(){
 
-        x += (Math.ceil(Math.random()*400)-200);
-        if(x<0)
-            x=0;
-        else if(x>window.innerWidth)
-            x = window.innerWidth;
-        y += (Math.ceil(Math.random()*400)-200);
-        if(y<0)
-            y=0;
-        else if(y>window.innerHeight)
-            y = window.innerHeight;
-        div.style.WebkitTransform = 'translate('+x+'px,'+y+'px)';
+        move.x = (Math.ceil(Math.random()*maxMove*2)-maxMove);
+        if(move.x<0)
+            move.x=0;
+        else if(move.x>window.innerWidth)
+            move.x = window.innerWidth;
+            
+        move.y = (Math.ceil(Math.random()*maxMove*2)-maxMove);
+        if(move.y<0)
+            move.y=0;
+        else if(move.y>window.innerHeight)
+            move.y = window.innerHeight;
+            
+        div.style.WebkitTransform = 'translate('+move.x+'px,'+move.y+'px)';
 
         setTimeout(function(){
             window.requestAnimationFrame(refresh,div);
-        },tempsTransition*1000-100);
+        },tempsTransition*1000-200);
       //  div.style.marginTop =  (getValueInCss(div.style.marginTop)+0.1)+"px";
       //  div.style.marginLeft =  (getValueInCss(div.style.marginLeft)+0.1)+"px";
     };
@@ -61,9 +73,17 @@ window.onload = function(){
 
     var bulleContainer = document.getElementById("bulleContainer");
 
-    var b = new bulle();
+      var bulles = [];
+     bulles.push(new bulle(window.innerWidth/3,window.innerHeight/2,100,8));
+     bulles.push(new bulle(window.innerWidth/5,window.innerHeight/4,100,10));
+     bulles.push(new bulle(window.innerWidth/1.3,window.innerHeight/4,100,12));
 
-    bulleContainer.appendChild(b.getElement());
+    for(var i=0;i<bulles.length;i++){
+      bulleContainer.appendChild(bulles[i].getElement());
+    }
+
+
+    
 
 
 
