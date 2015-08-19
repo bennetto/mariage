c09600161753faa2af3ffb5375e4a3474254bbbd
bennetto <bbonnetto@gmail.com>
2015-08-19 09:04:50 +0200

    Add click and scale

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ function bulle(_positionX,_positionY,_maxMove,temps){
     var container = document.createElement("div");
     
     var size = 70;
+    var sizeBig =600;
     var scale = 1;
     var position = {};
     position.x= _positionX;
@@ -58,25 +59,32 @@ function bulle(_positionX,_positionY,_maxMove,temps){
     container.style.borderRadius = size+"px";
 
     container.style.left = position.x+'px';
-    container.style.marginTop = position.y+'px';
+    container.style.top = position.y+'px';
 
     var changeTempsTransform = function(tmp){
 
-        container.style.WebkitTransition = '-webkit-transform '+tmp+'s ease-in-out,right '+1+'s ease-in-out,left '+1+'s ease-in-out';
+        container.style.WebkitTransition = '-webkit-transform '+tmp+'s ease-in-out,left '+1+'s ease-in-out,top '+1+'s ease-in-out,height '+1+'s ease-in-out,width '+1+'s ease-in-out,border-radius '+1+'s ease-in-out';
     };
     changeTempsTransform(tempsTransitionBulle);
 
     container.addEventListener('click',function(){
         bulleMode = !bulleMode;
         if(bulleMode){
-            changeTempsTransform(1);
-            scale = 1;
-            setTimeout(function() {
-                changeTempsTransform(tempsTransitionBulle);
-            },1000);
+            container.style.height = size+"px";
+            container.style.width = size+"px";
+            container.style.borderRadius = size+"px";
+
+            container.style.left = position.x - size/2+'px';
+            container.style.top = position.y - size/2+'px';
         }else{
-            changeTempsTransform(1);
-            scale = 10;
+            sizeBig = window.innerHeight-200;
+
+            container.style.height = sizeBig+"px";
+            container.style.width = sizeBig+"px";
+            container.style.borderRadius = sizeBig+"px";
+
+            container.style.left = (window.innerWidth -sizeBig) /2+'px';
+            container.style.top = (window.innerHeight - sizeBig)/2+'px';
         }
         refresh();
     });
@@ -98,9 +106,12 @@ function bulle(_positionX,_positionY,_maxMove,temps){
                 move.y = 0;
             else if (move.y > window.innerHeight)
                 move.y = window.innerHeight;
+            container.style.WebkitTransform = 'translate(' + move.x + 'px,' + move.y + 'px) scale('+scale+')';
+        }else{
+            container.style.WebkitTransform = 'translate(' + 0 + 'px,' + 0 + 'px) scale('+scale+')';
         }
         //write move
-        container.style.WebkitTransform = 'translate(' + move.x + 'px,' + move.y + 'px) scale('+scale+')';
+
 
         //add event next move
 
